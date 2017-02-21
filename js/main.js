4083e116e32bd3f9dd0cbd51339977f2e8960fe3
Sam Li <sam.li@tfgm.com>
2017-02-21 00:53:01 +0000

    Attempt 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 			$('#read').html(data);
 			//alert(data);
 			//$('#read').load(data);
-			$("#reader").html('<object type="text/html" data="'+data+'" ></object>');
+			document.getElementById("reader").innerHTML=
+			'<object type="text/html" style="width:100%;height:100%" data="'+data+'" ></object>';
 		},
 		function(error){
 			$('#read_error').html(error);
